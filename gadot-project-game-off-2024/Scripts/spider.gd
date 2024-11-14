5ef45be8aa3653d86d76192bdde4959d45501d9c
Emdove <emma.mross286@gmail.com>
2024-11-13 20:48:39 -0500

    Spider is ever so slightly working
    Going to revisit this little guy later. Movement is like super jank.

--- gadot-project-game-off-2024/Scripts/spider.gd
+++ gadot-project-game-off-2024/Scripts/spider.gd
@@ -1,7 +1,9 @@
 extends CharacterBody3D
 @export var payer_path :NodePath
 var player = null
-const speed = 20.0
+@export var speed = 10.0
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+@export var jump_speed = 20.0
 @onready var jump_timer: Timer = $JumpTimer
 @onready var active_timer: Timer = $ActiveTimer
 
@@ -14,15 +16,20 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
+	velocity.y += -gravity * delta
 	if jump_timer.is_stopped():
 		velocity = Vector3.ZERO
 		nav_agent.set_target_position(player.global_transform.origin)
 		var next_nav_point = nav_agent.get_next_path_position()
 		velocity = (next_nav_point - global_transform.origin).normalized() * speed
 		look_at(player.global_transform.origin)
-		move_and_slide()
+		if is_on_floor():
+			velocity.y =+ jump_speed
 		if active_timer.is_stopped():
 			jump_timer.start()
+			velocity = Vector3.ZERO
 	else:
 		active_timer.start()
+	
+	move_and_slide()
