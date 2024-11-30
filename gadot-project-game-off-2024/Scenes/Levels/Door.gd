08b0d85120d494a95f34729545c846c43f193a2e
Emdove <emma.mross286@gmail.com>
2024-11-30 02:35:19 -0500

    We have a rat problem

--- gadot-project-game-off-2024/Scenes/Levels/Door.gd
+++ gadot-project-game-off-2024/Scenes/Levels/Door.gd
@@ -4,8 +4,6 @@ extends MeshInstance3D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	SignalBus.connect("door_open",  Callable(self, "open_door"))
-	print(position)
-	print(rotation)
 
 
 func open_door():
