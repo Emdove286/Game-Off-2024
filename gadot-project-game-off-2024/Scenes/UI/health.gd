bc776eb94d5c8bc8bccca3d855a59c561dddf70f
Emdove <emma.mross286@gmail.com>
2024-12-01 14:17:55 -0500

    FreshBuild

--- gadot-project-game-off-2024/Scenes/UI/health.gd
+++ gadot-project-game-off-2024/Scenes/UI/health.gd
@@ -1,12 +1,16 @@
 extends CanvasLayer
 @export var player : Player
 @onready var health: Label = $Health
+@onready var keys: Label = $Keys
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	SignalBus.connect("add_keys",  Callable(self, "add_keys"))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	health.text = str(player.health)
+	
+func add_keys(keyamount):
+	keys.text = str(keyamount) + " out of 2 keys found"
--- gadot-project-game-off-2024/Scenes/UI/menu_overlay.gd
+++ gadot-project-game-off-2024/Scenes/UI/menu_overlay.gd
@@ -1,4 +1,6 @@
 extends CanvasLayer
 
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Levels/MainLevel.tscn")
